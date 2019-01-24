ec41855acc194ee8165abe2b3de899d891a148a0
srrjs <muhammad.rizwan@synavos.com>
2019-01-24 10:53:49 +0500

    basic starter kit with redux

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -2,13 +2,18 @@ import React from 'react'
 import { Route, Link } from 'react-router-dom'
 import Home from '../home'
 import About from '../about'
+import { Button, Layout, Row, Col } from 'antd';
+
+const {Header, Footer, Sider, Content} = Layout;
 
 const App = () => (
   <div>
-    <header>
-      <Link to="/">Home</Link>
-      <Link to="/about-us">About</Link>
-    </header>
+    <Layout>
+      <Header>
+        <Link to="/">Home </Link>
+        <Link to="/about-us"> About</Link>
+      </Header>
+    </Layout>
 
     <main>
       <Route exact path="/" component={Home} />
